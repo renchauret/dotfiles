7abea014c518e66299033d322fdf4fb5490e82b5
Ren Chauret <renaudchauret@gmail.com>
2023-07-24 11:53:18 -0400

    Update lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -30,9 +30,9 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
   ['<C-Space>'] = cmp.mapping.complete(),
 })
 
@@ -42,7 +42,7 @@ lsp.setup_nvim_cmp({
 
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set('n', 'gk', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set('n', 'gk', function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set('n', '<leader>n', function() vim.lsp.buf.rename() end, opts)
